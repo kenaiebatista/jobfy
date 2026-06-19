d2d36b1b32524f6df938d30d1f48e0f637a6d4f6
kenai1243 <kenaiebatista@gmail.com>
2026-06-18 21:24:08 -0300

    refactor: login, cadastro, tela inicial, tela perfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,21 @@
-import 'package:aplicativo_jobfy/screens/tela_Cadastro.dart';
-import 'package:aplicativo_jobfy/screens/tela_Login.dart';
-import 'package:aplicativo_jobfy/ui/util/headerPage.dart';
+import 'package:aplicativo_jobfy/core/routes/app_router.dart';
+import 'package:aplicativo_jobfy/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const JobfyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JobfyApp extends StatelessWidget {
+  const JobfyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Jobfy',
       debugShowCheckedModeBanner: false,
-      routerConfig: _router,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarThemeData(backgroundColor: Colors.black),
-      ),
-    );
-  }
-}
-
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const homePage();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'login',
-          builder: (BuildContext context, GoRouterState state) {
-            return const LoginPage();
-          },
-        ),
-        GoRoute(
-          path: 'register',
-          builder: (BuildContext context, GoRouterState state) {
-            return const RegisterPage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
-
-class homePage extends StatelessWidget {
-  const homePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: headerPage(), body: webPage());
-  }
-}
-
-class webPage extends StatefulWidget {
-  const webPage({super.key});
-
-  @override
-  State<webPage> createState() => _webPageState();
-}
-
-class _webPageState extends State<webPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Bem vindo, $nome',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Divider(color: Colors.grey, height: 100),
-        ],
-      ),
+      routerConfig: appRouter,
+      theme: AppTheme.theme,
     );
   }
 }
