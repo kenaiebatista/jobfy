1a70ab5776e4be0b8f581e2179ed142cc5c064fb
henriqueknebel <henriqueknebel08@gmail.com>
2026-09-01 09:00:17 -0300

    feature(): creation of the job directory

---
+++ lib/features/job/presentation/pages/job_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JobPage extends StatelessWidget {
+  const JobPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
